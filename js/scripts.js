6de60bf23563b3157b6596518ca09fd00609d4d8
Nicole Cox <nicolecox@Nicoles-MacBook-Pro.local>
2018-09-22 19:54:47 -0700

    working on css

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+var add = function(question1, question2, question3, question4, question5) {
+ return question1 + question2 + question3 + question4 + question5
+}
+
 $(document).ready(function() {
  $("form").submit(function(event) {
    event.preventDefault();
